0153febe04ff9ff259bb6e9038044445ef26fdc9
Sourabh Aryabhatta <sourabh46@yahoo.com>
2020-02-17 22:35:11 +0200

    Updated documentation for Assignment01
    Added necessary comments to describe methods and statements.

--- Assignment01/src/com/java/palindrome/Element.java
+++ Assignment01/src/com/java/palindrome/Element.java
@@ -14,32 +14,51 @@ public class Element
     private char item;
     private int count;
     
+    /**
+     * Constructs Element object.
+     */
     public Element()
     {
-        // Default constructor
+        // Empty Body
     }
-    
+    /**
+     * Constructs Element object.
+     * @param item Character of the Element object.
+     * @param count Number of characters of the Element object.
+     */
     public Element(char item, int count)
     {
         this.item = item;
         this.count = count;
     }
-    
+    /**
+     * Sets the character of the Element object.
+     * @param item Character to be set.
+     */
     public void setItem(char item)
     {
         this.item = item;
     }
-    
+    /**
+     * Returns the character of the Element object.
+     * @return Character of the Element object.
+     */
     public char getItem()
     {
         return item;
     }
-    
+    /**
+     * Sets the number of characters of the Element object.
+     * @param count Number of characters of the Element object.
+     */
     public void setCount(int count)
     {
         this.count = count;
     }
-    
+    /**
+     * Returns the number of characters of the Element object.
+     * @return Number of characters of the Element object.
+     */
     public int getCount()
     {
         return count;
--- Assignment01/src/com/java/palindrome/PalindromeChecker.java
+++ Assignment01/src/com/java/palindrome/PalindromeChecker.java
@@ -16,12 +16,21 @@ public class PalindromeChecker
 {
     public static void main(String[] args)
     {
+        /**
+         * Prompts user to enter text and reads input string.
+         */
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your string: ");
         String inputString = scanner.nextLine();
         
+        /**
+         * Initializes the list of Element objects.
+         */
         ArrayList<Element> elementList = new ArrayList<Element>();
         
+        /**
+         * Iterates through the characters of the input string.
+         */
         for(int index = 0; index < inputString.length(); index++)
         {
             boolean characterFoundFlag = false;
@@ -29,26 +38,42 @@ public class PalindromeChecker
             {
                 if(elementList.get(elementListIndex).getItem() == inputString.charAt(index))
                 {
+                    /**
+                     * Raises flag if the character already exists in the Element list and increases corresponding counter. 
+                     */
                     characterFoundFlag = true;
                     elementList.get(elementListIndex).setCount(elementList.get(elementListIndex).getCount()+1);
                     break;
                 }
             }
+            /**
+             * Adds new character to the Element list with counter value as 1.
+             */
             if (characterFoundFlag == false)
             {
                 elementList.add(new Element(inputString.charAt(index), 1));
             }
         }
-        
+        /**
+         * Prints 'true' if list of elements contains only one type of character.
+         */
         if(elementList.size() == 1)
         {
             System.out.println("true");
         }
         else
         {
+            /**
+             * Input string contains more than one type of characters.
+             * Type of characters can be either odd or even.
+             */
             int oddCounter = 0;
             if(inputString.length() % 2 != 0)
             {
+                /**
+                 * If length of input string is odd, then only one type of characters can be odd at maximum,
+                 * to meet the criteria of making at least one Palindrome.
+                 */
                 for(int elementListIndex = 0; elementListIndex < elementList.size(); elementListIndex++)
                 {
                     if(elementList.get(elementListIndex).getCount() % 2 == 0)
@@ -71,6 +96,10 @@ public class PalindromeChecker
             }
             else
             {
+                /**
+                 * If length of input string is even, then all type of characters MUST be even,
+                 * to meet the criteria of making at least one Palindrome.
+                 */
                 for(int elementListIndex = 0; elementListIndex < elementList.size(); elementListIndex++)
                 {
                     if(elementList.get(elementListIndex).getCount() % 2 == 0)
