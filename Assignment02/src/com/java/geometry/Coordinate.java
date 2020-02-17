a5ffd4b1a37cf3245a71ea8a3a8060b7b3905a6d
Sourabh Aryabhatta <sourabh46@yahoo.com>
2020-02-17 23:10:10 +0200

    Updated documentation for Assignment02/src/com/java/geometry/Coordinate
    Added necessary comments to describe methods and statements.

--- Assignment02/src/com/java/geometry/Coordinate.java
+++ Assignment02/src/com/java/geometry/Coordinate.java
@@ -11,47 +11,69 @@ package com.java.geometry;
  */
 public class Coordinate
 {
-
     private float x;
     private float y;
-
+    /**
+     * Constructs Coordinate object.
+     */
     public Coordinate()
     {
-        // Default Constructor
+        // Empty-body constructor
     }
-
+    /**
+     * Constructs Coordinate object.
+     * @param coordinate Coordinate object to be created.
+     */
     public Coordinate(Coordinate coordinate)
     {
         this.x = coordinate.getX();
         this.y = coordinate.getY();
     }
-
+    /**
+     * Constructs Coordinate object.
+     * @param x X value of the coordinate.
+     * @param y Y value of the coordinate.
+     */
     public Coordinate(float x, float y)
     {
         this.x = x;
         this.y = y;
     }
-
+    /**
+     * Sets the X value of Coordinate object.
+     * @param x X value.
+     */
     public void setX(float x)
     {
         this.x = x;
     }
-
+    /**
+     * Returns the X value of Coordinate object.
+     * @return X value.
+     */
     public float getX()
     {
         return x;
     }
-
+    /**
+     * Sets the Y value of Coordinate object.
+     * @param y Y value.
+     */
     public void setY(float y)
     {
         this.y = y;
     }
-
+    /**
+     * Returns the Y value of Coordinate object.
+     * @return Y value.
+     */
     public float getY()
     {
         return y;
     }
-
+    /**
+     * Prints the Coordinate object.
+     */
     public void print()
     {
         System.out.println("(" + Math.round(this.getX()) + "," + Math.round(this.getY()) + ")");
--- Assignment02/src/com/java/geometry/DrawLine.java
+++ Assignment02/src/com/java/geometry/DrawLine.java
@@ -16,6 +16,9 @@ public class DrawLine
 {
     public static void main(String[] args)
     {
+        /**
+         * Prompts user to input two points (x0, y0) and (x1, y1).
+         */
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter x0: ");
         float x0 = scanner.nextInt();
@@ -25,81 +28,113 @@ public class DrawLine
         float x1 = scanner.nextInt();
         System.out.print("Enter y1: ");
         float y1 = scanner.nextInt();
-
-        Coordinate initialPofloat = new Coordinate(x0, y0);
-        Coordinate finalPofloat = new Coordinate(x1, y1);
-
+        
+        /**
+         * Initializes initial point (x0, y0) and final point (x1, y1).
+         */
+        Coordinate initialPointFloat = new Coordinate(x0, y0);
+        Coordinate finalPointFloat = new Coordinate(x1, y1);
+        
+        /**
+         * Initializes the list of Coordinate objects.
+         */
         ArrayList<Coordinate> coordinateList = new ArrayList<Coordinate>();
 
-        if ((initialPofloat.getX() == finalPofloat.getX()) && (initialPofloat.getY() == finalPofloat.getY()))
+        if ((initialPointFloat.getX() == finalPointFloat.getX()) && (initialPointFloat.getY() == finalPointFloat.getY()))
         {
+            /**
+             * If initial point and final point represent the same coordinate.
+             */
             System.out.println("Sorry, (x0, y0) and (x1, y1) are pointing to the same co-ordinate");
         }
-        else if (initialPofloat.getX() == finalPofloat.getX())
+        else if (initialPointFloat.getX() == finalPointFloat.getX())
         {
-            if (initialPofloat.getY() < finalPofloat.getY())
+            /**
+             * If initial point and final point have same X value.
+             */
+            if (initialPointFloat.getY() < finalPointFloat.getY())
             {
-                for (float yValue = initialPofloat.getY(); yValue <= finalPofloat.getY(); yValue += 1)
+                for (float yValue = initialPointFloat.getY(); yValue <= finalPointFloat.getY(); yValue += 1)
                 {
-                    coordinateList.add(new Coordinate(initialPofloat.getX(), yValue));
+                    coordinateList.add(new Coordinate(initialPointFloat.getX(), yValue));
                 }
             }
             else
             {
-                for (float yValue = finalPofloat.getY(); yValue <= initialPofloat.getY(); yValue += 1)
+                for (float yValue = finalPointFloat.getY(); yValue <= initialPointFloat.getY(); yValue += 1)
                 {
-                    coordinateList.add(new Coordinate(initialPofloat.getX(), yValue));
+                    coordinateList.add(new Coordinate(initialPointFloat.getX(), yValue));
                 }
             }
         }
-        else if (initialPofloat.getY() == finalPofloat.getY())
+        else if (initialPointFloat.getY() == finalPointFloat.getY())
         {
-            if (initialPofloat.getX() < finalPofloat.getX())
+            /**
+             * If initial point and final point have same Y value.
+             */
+            if (initialPointFloat.getX() < finalPointFloat.getX())
             {
-                for (float xValue = initialPofloat.getX(); xValue <= finalPofloat.getX(); xValue += 1)
+                for (float xValue = initialPointFloat.getX(); xValue <= finalPointFloat.getX(); xValue += 1)
                 {
-                    coordinateList.add(new Coordinate(xValue, initialPofloat.getY()));
+                    coordinateList.add(new Coordinate(xValue, initialPointFloat.getY()));
                 }
             }
             else
             {
-                for (float xValue = finalPofloat.getX(); xValue <= initialPofloat.getX(); xValue += 1)
+                for (float xValue = finalPointFloat.getX(); xValue <= initialPointFloat.getX(); xValue += 1)
                 {
-                    coordinateList.add(new Coordinate(xValue, initialPofloat.getY()));
+                    coordinateList.add(new Coordinate(xValue, initialPointFloat.getY()));
                 }
             }
         }
         else
         {
-            float initialXValue = (finalPofloat.getX() > initialPofloat.getX()) ? initialPofloat.getX() : finalPofloat.getX();
-            float maxXValue = (finalPofloat.getX() > initialPofloat.getX()) ? finalPofloat.getX() : initialPofloat.getX();
+            /**
+             * If initial point and final point have different X and Y values.
+             */
+            float initialXValue = (finalPointFloat.getX() > initialPointFloat.getX()) ? initialPointFloat.getX() : finalPointFloat.getX();
+            float maxXValue = (finalPointFloat.getX() > initialPointFloat.getX()) ? finalPointFloat.getX() : initialPointFloat.getX();
 
-            float initialYValue = (finalPofloat.getY() > initialPofloat.getY()) ? initialPofloat.getY() : finalPofloat.getY();
-            float maxYValue = (finalPofloat.getY() > initialPofloat.getY()) ? finalPofloat.getY() : initialPofloat.getY();
-
-            for (float xValue = initialXValue; xValue <= maxXValue; xValue += 0.01)
+            float initialYValue = (finalPointFloat.getY() > initialPointFloat.getY()) ? initialPointFloat.getY() : finalPointFloat.getY();
+            float maxYValue = (finalPointFloat.getY() > initialPointFloat.getY()) ? finalPointFloat.getY() : initialPointFloat.getY();
+            
+            float delta = (float)(0.01);
+            
+            /**
+             * Iterates through list of possible coordinates with a delta step-increment for both X value and Y value.
+             */
+            for (float xValue = initialXValue; xValue <= maxXValue; xValue += delta)
             {
-                for (float yValue = initialYValue; yValue <= maxYValue; yValue += 0.01)
+                for (float yValue = initialYValue; yValue <= maxYValue; yValue += delta)
                 {
-                    if (lineEquation(initialPofloat.getX(), initialPofloat.getY(), finalPofloat.getX(), finalPofloat.getY(), xValue, yValue) == 0)
+                    if (lineEquation(initialPointFloat.getX(), initialPointFloat.getY(), finalPointFloat.getX(), finalPointFloat.getY(), xValue, yValue) == 0)
                     {
+                        /**
+                         * Prepares list of coordinates that satisfies the equation of straight line.
+                         */
                         coordinateList.add(new Coordinate(xValue, yValue));
                     }
                 }
             }
         }
+        /**
+         * Initializes list of interpolated (final) coordinates.
+         */
         ArrayList<Coordinate> interpolatedCoordinateList = new ArrayList<Coordinate>();
 
         float x = 0;
         float y = 0;
-
-        for (int i = 0; i < coordinateList.size(); i++)
+        
+        /**
+         * Iterates through the list of coordinates to discard duplicate coordinates.
+         */
+        for (int indexOfCoordinateList = 0; indexOfCoordinateList < coordinateList.size(); indexOfCoordinateList++)
         {
-            x = (float) (Math.round(coordinateList.get(i).getX()));
-            y = (float) (Math.round(coordinateList.get(i).getY()));
+            x = (float) (Math.round(coordinateList.get(indexOfCoordinateList).getX()));
+            y = (float) (Math.round(coordinateList.get(indexOfCoordinateList).getY()));
 
             boolean isCoordinateAlreadyAddedFlag = false;
-            int j = 0;
+            int iterator = 0;
             do
             {
                 if (interpolatedCoordinateList.size() == 0)
@@ -109,30 +144,40 @@ public class DrawLine
                 }
                 else
                 {
-                    if (interpolatedCoordinateList.get(j).getX() == x && interpolatedCoordinateList.get(j).getY() == y)
+                    if (interpolatedCoordinateList.get(iterator).getX() == x && interpolatedCoordinateList.get(iterator).getY() == y)
                     {
                         isCoordinateAlreadyAddedFlag = true;
                         break;
                     }
-                    j++;
+                    iterator++;
                 }
             }
-            while (j < interpolatedCoordinateList.size());
+            while (iterator < interpolatedCoordinateList.size());
             if (isCoordinateAlreadyAddedFlag == false)
             {
                 interpolatedCoordinateList.add(new Coordinate(x, y));
             }
         }
-
+        /**
+         * Prints the final coordinate list.
+         */
         for (int index = 0; index < interpolatedCoordinateList.size(); index++)
         {
             interpolatedCoordinateList.get(index).print();
         }
     }
-
+    /**
+     * Returns 0 if (x, y) is situated on the straight line between (x0, y0) and (x1, y1).
+     * @param x0 X value of coordinate (x0, y0).
+     * @param y0 Y value of coordinate (x0, y0).
+     * @param x1 X value of coordinate (x1, y1).
+     * @param y1 Y value of coordinate (x1, y1).
+     * @param x X value of coordinate (x, y).
+     * @param y Y value of coordinate (x, y).
+     * @return 
+     */
     public static long lineEquation(float x0, float y0, float x1, float y1, float x, float y)
     {
         return Math.round((x - x0) * (y0 - y1) - (y - y0) * (x0 - x1));
-        //return Math.round((float)(x - x0)/(float)(x0 - x1) - (float)(y - y0)/(float)(y0 - y1));
     }
 }
